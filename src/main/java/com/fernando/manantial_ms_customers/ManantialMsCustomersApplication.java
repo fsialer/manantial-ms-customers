8020fe61ba86585f0d898226b56771b64b9d6189
Fernando <asialer05@hotmail.com>
2025-07-12 15:16:36 -0500

    Project base initial

---
+++ src/main/java/com/fernando/manantial_ms_customers/ManantialMsCustomersApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.manantial_ms_customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManantialMsCustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManantialMsCustomersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/manantial_ms_customers/ManantialMsCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.manantial_ms_customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManantialMsCustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
